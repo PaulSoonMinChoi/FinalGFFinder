593745d77b1f360bb510fd50360cf65c660cc110
Tracy <ymailingtracy@yahoo.com>
2020-11-03 18:22:22 -0800

    Completed sign up module

--- client/src/SignUpModal.jsx
+++ client/src/SignUpModal.jsx
@@ -1,17 +1,109 @@
 import React from 'react';
 
-const SignUpModal = ({ handleClose, show, children }) => {
-  const showHideClassName = show ? "modal display-block" : "modal display-none";
+class SignUpModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playertype: '',
+      fname: '',
+      lname: '',
+      username: '',
+      password: '',
+      region: '',
+      aboutme: '',
+    }
+    this.handlePlayerTypeChange = this.handlePlayerTypeChange.bind(this);
+    this.handleRegionChange = this.handleRegionChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  return (
-    <div className={showHideClassName}>
-      <section className="sign-up-modal-main">
-        <div className="ttsignupTitle">Let's find you some friends!</div>
+  handlePlayerTypeChange(e){
 
-        <div className="ttclose" onClick={handleClose}>x</div>
-      </section>
-    </div>
-  );
-};
+  }
+  handleRegionChange(e){
+
+  }
+  handleChange(e){
+    const target = e.target;
+    const name = target.name;
+
+    this.setState({
+      [name]: target.value
+    });
+    console.log([name]);
+  }
+
+  render() {
+    const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
+    return (
+      <div className={showHideClassName}>
+        <section className="sign-up-modal-main">
+          <div className="ttsignupTitle">Let's find you some friends!</div>
+          <br></br>
+          <div>
+          <form classname="ttsignupform">
+            <label>
+              First Name:
+              <br></br>
+            <input classname="ttname" type="text" name="fname" onChange={this.handleChange} />
+            </label>
+            <br></br>
+            <label>
+              Last Name:
+              <br></br>
+            <input classname="ttname" type="text" name="lname" onChange={this.handleChange} />
+            </label>
+
+            <br></br>
+            <label>
+              Username:
+              <br></br>
+            <input classname="textinput" type="text" name="username" onChange={this.handleChange} />
+            </label>
+            <br></br>
+            <label>
+              Password:
+              <br></br>
+            <input classname="textinput" type="text" name="password" onChange={this.handleChange} />
+            </label>
+            <br></br>
+            <label>
+              Region:
+            <select value={this.state.region} onChange={this.handleRegionChange}>
+                <option value="NA">NA</option>
+                <option value="UK">United Kingom</option>
+                <option value="LAN">LAN</option>
+                <option value="Asia">Asia</option>
+              </select>
+            </label>
+            <br></br>
+            <label>
+              Player Type:
+            <select value={this.state.playertype} onChange={this.handlePlayerTypeChange}>
+                <option value="casual">Casual</option>
+                <option value="hardcorerank">Hardcore Rank</option>
+                <option value="learning">Learning</option>
+              </select>
+            </label>
+            <br></br>
+            <label>
+              About Me:
+              <br></br>
+            <input classname="inputtext" className="aboutme" type="text" name="aboutme" onChange={this.handleChange} />
+            </label>
+            <br></br>
+
+            <input type="submit" value="Submit" />
+          </form>
+          </div>
+
+          <div className="ttclosesignup" onClick={this.props.handleClose}>I changed my mind...</div>
+        </section>
+      </div>
+    );
+  };
+
+  }
+
 
 export default SignUpModal;
