e513ce211b00cbce06b33f774cf3c85aeaab127d
Justin Murakami <jmurakami84@gmail.com>
2020-11-08 07:54:28 -0800

    Added set interval to refresh friends list every five seconds. Does not cause popover modal friend details to reload if open (so that's good). Made height of right and left modules consistent with center module (520px) in stylesheet, and fixed to scroll if friend or team list is longer than height of module, and hid scrolling bar.

--- client/src/justinsComponents/RightModule/Team.jsx
+++ client/src/justinsComponents/RightModule/Team.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const TeamBlock = styled.div`
+  width: 100%;
+  display: in-line block;
+  text-align: center;
+`;
+
+const TeamName = styled.h3`
+  display: block;
+  margin-top: 10px;
+  margin-bottom: 2px;
+  font-size: 16px;
+  color: #FFF;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const TeamNumber = styled.p`
+  display: block;
+  margin-top: 1px;
+  font-size: 14px;
+  color: #FFF;
+`;
+
 export default function Team(props) {
 
-  const TeamBlock = styled.div`
-    width: 100%;
-    display: in-line block;
-    text-align: center;
-  `;
-
-  const TeamName = styled.h3`
-    display: block;
-    margin-top: 10px;
-    margin-bottom: 2px;
-    font-size: 16px;
-    color: #FFF;
-    &:hover {
-      cursor: pointer;
-    }
-  `;
-
-  const TeamNumber = styled.p`
-    display: block;
-    margin-top: 1px;
-    font-size: 14px;
-    color: #FFF;
-  `;
 
   return (
     <TeamBlock onClick={() => {props.handleClickTeamDetail(props.team.ID)}}>
